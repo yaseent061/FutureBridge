dcf46fda369dbd685e452ee135c5f0b0c7facc70
prinson <prinson@aletheatech.com>
2023-10-13 14:42:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
 
 function App() {
+  const [boxState,setBoxState] = useState([false,false,false])
+
+  // useEffect(()=>{
+  //   console.log(boxState)
+  // },[boxState])
+
+  const handleChange = (e ,ind)=>{
+    const temp = [...boxState];
+    console.log(ind,boxState ,"ind , boxstate")
+    console.log(e.target.checked,"check")
+    if(e.target.checked){
+      temp[ind] = true
+      setBoxState([...temp])
+    }
+    else
+      temp[ind] = false
+      setBoxState(temp)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {boxState.map((ele, ind)=>{
+        return(
+          <div> 
+            <input type="checkbox" onChange={(e)=>handleChange(e,ind)
+              } value={boxState}></input>
+              <span style={{marginLeft: "20px"}}>{boxState[ind]?"On":"Off"}</span>
+          </div>
+        )
+      })}
+     
     </div>
   );
 }
